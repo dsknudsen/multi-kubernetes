de7ceb46635738c7edc78e5ef991f9f1c965f0b9
dsknudsen <dsknudsen@gmail.com>
2021-02-24 14:08:53 -0700

    pushed fixed to trigger GCP redeploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ docker build -t dsknudsen/multi-client:latest -t dsknudsen/multi-client:$SHA -f 
 docker build -t dsknudsen/multi-server:latest -t dsknudsen/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t dsknudsen/multi-worker:latest -t dsknudsen/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push dsknudsen/multi-client:latest
-docker push dsknudsen/multi-client:$SHA
 docker push dsknudsen/multi-server:latest
-docker push dsknudsen/multi-server:$SHA
 docker push dsknudsen/multi-worker:latest
+docker push dsknudsen/multi-client:$SHA
+docker push dsknudsen/multi-server:$SHA
 docker push dsknudsen/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=dsknudsen/multi-client:$SHA
